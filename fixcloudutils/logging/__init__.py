513456b0373299391196b339ef1ab246151bb44c
Matthias Veit <aquamatthias@users.noreply.github.com>
2023-10-02 20:33:54 +0200

    [feat] Add json log formatter with prometheus counter (#14)

---
+++ fixcloudutils/logging/__init__.py
@@ -0,0 +1,62 @@
+#  Copyright (c) 2023. Some Engineering
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import logging
+import os
+from logging import StreamHandler, basicConfig
+from typing import Optional, List
+
+from .json_logger import JsonFormatter
+from .prometheus_counter import PrometheusLoggingCounter
+
+__all__ = ["setup_logger", "JsonFormatter", "PrometheusLoggingCounter"]
+
+
+def setup_logger(
+    component: str,
+    *,
+    force: bool = True,
+    level: Optional[int] = None,
+    json_format: bool = True,
+    count_logs: bool = True,
+    log_format: Optional[str] = None,
+) -> List[StreamHandler]:  # type: ignore
+    log_level = level or logging.INFO
+    # override log output via env var
+    plain_text = os.environ.get("LOG_TEXT", "false").lower() == "true"
+    handler = PrometheusLoggingCounter(component) if count_logs else StreamHandler()
+    if json_format and not plain_text:
+        formatter = JsonFormatter(
+            {
+                "timestamp": "asctime",
+                "level": "levelname",
+                "message": "message",
+                "pid": "process",
+                "thread": "threadName",
+            },
+            static_values={"component": component},
+        )
+        handler.setFormatter(formatter)
+        basicConfig(handlers=[handler], force=force, level=log_level)
+    else:
+        lf = log_format or "%(asctime)s %(levelname)s %(message)s"
+        basicConfig(handlers=[handler], format=lf, datefmt="%Y-%m-%dT%H:%M:%S", force=force, level=log_level)
+    return [handler]
---
+++ fixcloudutils/logging/json_logger.py
@@ -0,0 +1,61 @@
+#  Copyright (c) 2023. Some Engineering
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import json
+from logging import Formatter, LogRecord
+from typing import Mapping, Optional, Dict
+
+
+class JsonFormatter(Formatter):
+    """
+    Simple json log formatter.
+    """
+
+    def __init__(
+        self,
+        fmt_dict: Mapping[str, str],
+        time_format: str = "%Y-%m-%dT%H:%M:%S",
+        static_values: Optional[Dict[str, str]] = None,
+    ) -> None:
+        super().__init__()
+        self.fmt_dict = fmt_dict
+        self.time_format = time_format
+        self.static_values = static_values or {}
+        self.__uses_time = "asctime" in self.fmt_dict.values()
+
+    def usesTime(self) -> bool:  # noqa: N802
+        return self.__uses_time
+
+    def format(self, record: LogRecord) -> str:
+        record.message = record.getMessage()
+        if self.__uses_time:
+            record.asctime = self.formatTime(record, self.time_format)
+
+        message_dict = {fmt_key: record.__dict__[fmt_val] for fmt_key, fmt_val in self.fmt_dict.items()}
+        message_dict.update(self.static_values)
+        if record.exc_info:
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        if record.exc_text:
+            message_dict["exception"] = record.exc_text
+        if record.stack_info:
+            message_dict["stack_info"] = self.formatStack(record.stack_info)
+        return json.dumps(message_dict, default=str)

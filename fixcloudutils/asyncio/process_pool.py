1fbfc2cbb5eae8c6cc4d85fdd785475b884e8b98
Matthias Veit <matthias_veit@yahoo.de>
2024-04-23 08:31:09 +0200

    [fix] AsyncPool kwargs

--- fixcloudutils/asyncio/process_pool.py
+++ fixcloudutils/asyncio/process_pool.py
@@ -15,6 +15,8 @@ import asyncio
 import logging
 from concurrent.futures import ProcessPoolExecutor
 from concurrent.futures.process import BrokenProcessPool
+from datetime import timedelta
+from functools import partial
 from typing import Optional, Any, Callable, TypeVar
 
 from fixcloudutils.service import Service
@@ -24,10 +26,11 @@ T = TypeVar("T")
 
 
 class AsyncProcessPool(Service):
-    def __init__(self, **pool_args: Any):
+    def __init__(self, timeout: Optional[timedelta] = None, **pool_args: Any):
         self.pool_args = pool_args
         self.executor: Optional[ProcessPoolExecutor] = None
         self.lock = asyncio.Lock()
+        self.timeout = timeout.total_seconds() if timeout else None
 
     async def start(self) -> Any:
         async with self.lock:
@@ -40,12 +43,13 @@ class AsyncProcessPool(Service):
                 self.executor.shutdown(wait=True)
                 self.executor = None
 
-    async def submit(self, func: Callable[..., T], *args: Any, timeout: Optional[float] = None) -> T:
+    async def submit(self, func: Callable[..., T], *args: Any, **kwargs: Any) -> T:
         assert self.executor is not None, "Executor not started"
         loop = asyncio.get_running_loop()
         executor_id = id(self.executor)
         try:
-            return await asyncio.wait_for(loop.run_in_executor(self.executor, func, *args), timeout=timeout)
+            fn = func if not kwargs else partial(func, **kwargs)
+            return await asyncio.wait_for(loop.run_in_executor(self.executor, fn, *args), timeout=self.timeout)  # type: ignore # noqa
         except BrokenProcessPool:  # every running task will raise this exception
             async with self.lock:
                 if id(self.executor) == executor_id:  # does this exception comes from the current executor?
